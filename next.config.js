6dd72c6718a5452be12221d76bbc38cfb6da80dc
Lagz0ne <duc@outlook.sg>
2023-04-29 17:53:07 +0700

    chore: update ci

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
 
-const isProduction = process.env.NODE_ENV === "production";
-const assetPrefix = isProduction ? "/site" : "";
- 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
+
 module.exports = withNextra({
   images: {
     unoptimized: true,
@@ -14,5 +29,5 @@ module.exports = withNextra({
   basePath: assetPrefix,
   reactStrictMode: true,
   swcMinify: true,
-  trailingSlash: true
-})
+  trailingSlash: true,
+});
