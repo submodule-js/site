d4c453de2e0c9a04eeb3c2ec1ad7235b0c3a30ba
Duke <duc@silentium.io>
2023-05-31 04:15:42 +0000

    updated readme for version 4

--- examples/basic-exec/main.ts
+++
@@ -1,9 +0,0 @@
-import { execute, prepare } from "./submodule";
-
-execute(({ services }, input) => {
-  services.log(input)
-}, { hello: 'world' })
-
-const log = prepare(({ services }, input: string) => {
-  services.log(input)
-})
